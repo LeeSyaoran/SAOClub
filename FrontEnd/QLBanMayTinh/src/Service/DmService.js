ce3d0e572143ee2b548cd9c4ac9b9f68c5fa708b
LeeSyaoran~ <leesyaoran01041307@gmail.com~>
2026-07-20 17:14:21 +0700

    feat: add CRUD functions to DmService for CPU/RAM/GPU/O-cung categories

--- FrontEnd/QLBanMayTinh/src/Service/DmService.js
+++ FrontEnd/QLBanMayTinh/src/Service/DmService.js
@@ -1,5 +1,4 @@
-import { get } from './api.js';
-
+import { get, post, put, del } from './api.js';
 export const getThuongHieu = () => get('/api/thuong-hieu');
 export const getNhaCungCap = () => get('/api/nha-cung-cap');
 export const getChucVu     = () => get('/api/chuc-vu');
@@ -7,3 +6,14 @@ export const getCpu        = () => get('/api/dm-cpu');
 export const getRam        = () => get('/api/dm-ram');
 export const getOCung      = () => get('/api/dm-o-cung');
 export const getGpu        = () => get('/api/dm-gpu');
+
+const crud = (path) => ({
+  getAll: () => get(`/api/${path}`),
+  save: (id, body) => id ? put(`/api/${path}/update/${id}`, body) : post(`/api/${path}`, body),
+  remove: (id) => del(`/api/${path}/delete/${id}`),
+});
+
+export const DmCpuService = crud('dm-cpu');
+export const DmRamService = crud('dm-ram');
+export const DmGpuService = crud('dm-gpu');
+export const DmOCungService = crud('dm-o-cung');
